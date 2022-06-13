792642ccc3cba845a153b7ffa2bbe36fc9e14f17
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-06-13 16:03:23 +0300

    hanoiTower added

---
+++ hanoiTower.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class hanoiTower {
+    public static void fromTo(ArrayList<Integer> from, ArrayList<Integer> to, ArrayList<Integer> mid, int q) {
+        
+        
+        
+    }
+
+
+}
