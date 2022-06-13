f9ae5e1b3d0a7f3670ed8afd6e50dc91cdb152f8
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-06-13 16:15:20 +0300

    hanoiTower ex added

--- hanoiTower.java
+++ hanoiTower.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 
 public class hanoiTower {
-    public static void fromTo(ArrayList<Integer> from, ArrayList<Integer> to, ArrayList<Integer> mid, int q) {
-        
-        
-        
+    public static void moveRing(ArrayList<Integer> from, ArrayList<Integer> buf, ArrayList<Integer> to,
+    int ring) {
+                
     }
-
-
+    
 }
